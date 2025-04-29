018c022e36a17388f5eb19d5a1f6605a5eabfd02
novvedsama <novvedsama@gmail.com>
2025-04-28 23:26:34 -0700

    scenes update

---
+++ Assets/NextSceneScript/FinishPoint.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //go to next level
+        }
+    }
+
+    
+}
